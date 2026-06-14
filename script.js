e5f743bbf85cd8993bb504a07c185b7a7238fa0d
thedevarashetty <thedevarashetty1@gmail.com>
2026-06-14 16:19:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -146,3 +146,20 @@
     });
   });
 })();
+
+
+window.addEventListener("scroll", () => {
+
+    const nav = document.querySelector(".nav-wrap");
+
+    if(window.scrollY > 50){
+
+        nav.classList.add("scrolled");
+
+    }else{
+
+        nav.classList.remove("scrolled");
+
+    }
+
+});
